7680896ebf4070571500ed168f0bfa83b274e238
Elena <73384917+e-danilova-327@users.noreply.github.com>
2021-09-13 13:25:51 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-/*Create beautiful UI to play music stored in the "music folder" using the HTML5 audio APIProject Specifications
-Create UI for music player including spinning image and song detail popup
-Add play and pause functionality
-Switch songs
-Progress bar*/
-
-// Create audio element
-var audio = document.createElement('audio');
